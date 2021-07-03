4f07766f8c9b1821d62fba0a5ab64e89cee5b3dd
ruoyushang <r.y.shang@gmail.com>
2021-07-02 19:57:17 -0700

    add GetRunL3Rates.py

---
+++ GetRunL3Rates.py
@@ -0,0 +1,126 @@
+
+import os
+import sys,ROOT
+import array
+import math
+from array import *
+from ROOT import *
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import ICRS, Galactic, FK4, FK5  # Low-level frames
+from scipy import special
+
+RunNumber = 0
+Elev = 0
+Azim = 0
+PedVar_DC = 0
+PedVar_PE = 0
+T1_RA = 0
+T1_Dec = 0
+T2_RA = 0
+T2_Dec = 0
+T3_RA = 0
+T3_Dec = 0
+T4_RA = 0
+T4_Dec = 0
+FIR_Mean = 0.
+FIR_RMS = 0.
+L3_rate = 0.
+Livetime = 0.
+
+List_RunNumber = []
+List_Elev = []
+List_Azim = []
+List_PedVar_DC = []
+List_PedVar_PE = []
+List_T1_RA = []
+List_T1_Dec = []
+List_T2_RA = []
+List_T2_Dec = []
+List_T3_RA = []
+List_T3_Dec = []
+List_T4_RA = []
+List_T4_Dec = []
+List_FIR_Mean = []
+List_FIR_RMS = []
+List_L3_rate = []
+List_Livetime = []
+
+Source_RunNumber = []
+Source_PedVar_DC = []
+Source_Elev = []
+Source_Azim = []
+Source_Livetime = []
+
+#inputFile = open('diagnostics.txt')
+inputFile = open('diagnostics_20210308.txt')
+for line in inputFile:
+    if line.split(' ')[0]=="#": 
+        #print 'this is a comment line'
+        continue
+    if len(line.split(' '))<40: continue
+    V0 = False
+    V1 = False
+    V2 = False
+    if line.split(' ')[1]=="0": 
+        #print 'this is a V0 line'
+        V0 = True
+    if line.split(' ')[1]=="1": 
+        #print 'this is a V1 line'
+        V1 = True
+    if line.split(' ')[1]=="2": 
+        #print 'this is a V2 line'
+        V2 = True
+    RunNumber = line.split(' ')[1-1]
+    List_RunNumber += [int(RunNumber)]
+    Elev = line.split(' ')[8-1]
+    Azim = line.split(' ')[9-1]
+    List_Elev += [float(Elev)]
+    List_Azim += [float(Azim)]
+    L3_rate = line.split(' ')[12-1]
+    List_L3_rate += [float(L3_rate)]
+    Livetime = line.split(' ')[11-1]
+    List_Livetime += [float(Livetime)]
+    if V1 or V2:
+        T1_RA = line.split(' ')[46-1]
+        T1_Dec = line.split(' ')[47-1]
+        T2_RA = line.split(' ')[46-1+14]
+        T2_Dec = line.split(' ')[47-1+14]
+        T3_RA = line.split(' ')[46-1+28]
+        T3_Dec = line.split(' ')[47-1+28]
+        T4_RA = line.split(' ')[46-1+42]
+        T4_Dec = line.split(' ')[47-1+42]
+        List_T1_RA += [float(T1_RA)]
+        List_T1_Dec += [float(T1_Dec)]
+        List_T2_RA += [float(T2_RA)]
+        List_T2_Dec += [float(T2_Dec)]
+        List_T3_RA += [float(T3_RA)]
+        List_T3_Dec += [float(T3_Dec)]
+        List_T4_RA += [float(T4_RA)]
+        List_T4_Dec += [float(T4_Dec)]
+    else:
+        List_T1_RA += [0.]
+        List_T1_Dec += [0.]
+        List_T2_RA += [0.]
+        List_T2_Dec += [0.]
+        List_T3_RA += [0.]
+        List_T3_Dec += [0.]
+        List_T4_RA += [0.]
+        List_T4_Dec += [0.]
+    if V2:
+        PedVar_DC = line.split(' ')[104-1]
+        PedVar_PE = line.split(' ')[105-1]
+        List_PedVar_DC += [float(PedVar_DC)]
+        List_PedVar_PE += [float(PedVar_PE)]
+        FIR_Mean = line.split(' ')[106-1]
+        FIR_RMS = line.split(' ')[107-1]
+        List_FIR_Mean += [float(FIR_Mean)]
+        List_FIR_RMS += [float(FIR_RMS)]
+    else:
+        List_PedVar_DC += [0.]
+        List_PedVar_PE += [0.]
+        List_FIR_Mean += [0.]
+        List_FIR_RMS += [0.]
+
+    print '%s %s'%(RunNumber,L3_rate)
+
