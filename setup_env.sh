f2de2778bd275438b563aa8d948551f76132cc6c
ruoyushang <r.y.shang@gmail.com>
2021-07-05 10:26:32 -0700

    add hoffman2 env variables

--- setup_env.sh
+++ setup_env.sh
@@ -2,3 +2,7 @@ export SMI_INPUT=/veritas/userspace/rshang/analysis/Results/v483
 export SMI_DIR=/home/rshang/MatrixDecompositionMethod
 export SMI_OUTPUT=/home/rshang/MatrixDecompositionMethod/output_nominal
 source /home/rshang/EventDisplay/root/bin/thisroot.sh
+
+#export SMI_INPUT=/u/scratch/r/rshang/EventDisplay_output
+#export SMI_DIR=/u/home/r/rshang/MatrixDecompositionMethod
+#export SMI_OUTPUT=/u/home/r/rshang/MatrixDecompositionMethod/output_nominal
